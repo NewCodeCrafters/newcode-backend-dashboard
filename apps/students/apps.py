c090fc1f87f0346edd3690ccaf1c9c8372f4c06c
divine122 <ogochukwudivine221@gmail.com>
2025-10-22 10:09:59 +0100

    resolved merge conflict again

--- apps/students/apps.py
+++ apps/students/apps.py
@@ -1,11 +1,5 @@
 from django.apps import AppConfig
 
 class StudentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.students'
-
-
-    def ready(self):
-        import apps.students.signals
-
-    
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.students"
